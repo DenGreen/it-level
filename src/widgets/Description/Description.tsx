dac5fa37227f3f145a1886bd1843715c66ef0f70
Denis Malyshev <malyshev_denis@outlook.com>
2023-06-25 19:58:00 +0400

    feat: изменил блок с описание

--- src/widgets/Description/Description.tsx
+++ src/widgets/Description/Description.tsx
@@ -1,9 +1,12 @@
 import style from "./style.module.scss";
+import Image from "next/image";
 import { fonts } from "@/fonts/fonts";
+import it from "@/img/IT1-desc.jpg"
 
 export const Description = () => {
   return (
     <section className={style.box}>
+      <Image src={it} className={style.img} alt={"Фото человека за ремоньом ноутбука"} />
       <p className={`${fonts.body3.className} ${style.text}`}>
         Компьютеры, ноутбуки - техника современная и не застрахована от поломок,
         еслы ваше устройство вышло из строя, вы всегда можете рассчитывать на
